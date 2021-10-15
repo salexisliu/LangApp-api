0cdc4badf3583031373e3ffaeeb8a36064eb6f7f
Alexis S Liu <syl1350@gmail.com>
2021-10-14 20:54:02 -0400

    set up initial JWT auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-  def create
-    @user = User.create(user_params)
-    if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
-    else
-      render json: { error: 'failed to create user' }, status: :unprocessable_entity
-    end
+  skip_before_action :authorized, only: [:create]
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
   def index
@@ -13,8 +10,20 @@ class Api::V1::UsersController < ApplicationController
     render json: @users
   end
 
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
+    end
+  end
+
   private
+
   def user_params
-    params.require(:user).permit(:username, :name, :password, :bio, :avatar)
+    params.require(:user).permit(:username, :password, :bio, :avatar)
   end
 end
